ab4ef745bcdb765044d30f2e30ee5fd6e547dbc4
ImamIfti056 <imamul.ifti@gmail.com>
2021-10-04 22:24:17 +0600

    404m

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            <h3>404 page not found</h3>
+        <div className='text-center my-5 py-5'>
+            <h1 className='text-danger my-3'>404 Page not Found!</h1>
+            <p>The page you are looking for has incorrect url or check your internet connection.</p>
         </div>
     );
 };
